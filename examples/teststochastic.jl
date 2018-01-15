acfa2570b7faa699cd80aa4dd194eca29f00de60
glwagner <wagner.greg@gmail.com>
2018-01-15 10:17:47 -0500

    Updates

--- examples/teststochastic.jl
+++ examples/teststochastic.jl
@@ -3,7 +3,8 @@ using TurbulenceTools
 for ki = [8, 16]
   for μ = [0, 1e-2]
 
-    runstochasticforcingproblem(tf=10, dt=5e-3, withplot=true, ns=10, n=256,
+    # 1/ν = 200...
+    runstochasticforcingproblem(tf=200, dt=5e-3, withplot=true, ns=10, n=256,
       ν=5e-3, μ=μ, stepper="FilteredRK4")
 
   end
