fa38c92c3682ff0c8e0ecd9e9c3b286efba57ea0
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2018-01-15 22:48:49 -0500

    Working on test

--- examples/teststochastic.jl
+++ examples/teststochastic.jl
@@ -1,21 +1,20 @@
 using TurbulenceTools
 
-n = 128
-ν = 5e-3
-tf = 10/ν
+ n = 256
+ ν = 1e-6
+tf = 20
 
-for ki = [8, 16]
-  for μ = [0, 1e-2]
+fi = 1.0
+ki = 32
+ μ = 1e1
 
-    # 1/1e-2 = 100...
-    # 1/5e-3 = 200...
-    # 1/2e-3 = 500...
+# 1/1e-2 = 100...
+# 1/5e-3 = 200...
+# 1/2e-3 = 500...
 
-    @printf "Running with ki=%d and μ=%4.0e...\n" ki μ
+@printf "Running with fi=%.2f, ki=%d, and μ=%4.0e...\n" fi ki μ
 
-    plotname = @sprintf("teststochastic_n%d_ki%02d_mu%4.0e", n, ki, μ)
-    runstochasticforcingproblem(tf=tf, dt=1e-2, withplot=true, ns=10, n=n,
-      ν=ν, μ=μ, stepper="FilteredRK4", plotname=plotname)
+plotname = @sprintf("forcing_fi%02d_n%d_ki%02d_mu%4.0e", Int(fi*10), n, ki, μ)
 
-  end
-end
+runstochasticforcingproblem(fi=fi, ki=ki, tf=tf, dt=5e-3, withplot=true, 
+  ns=10, n=n, ν=ν, μ=μ, stepper="FilteredRK4", plotname=plotname)
